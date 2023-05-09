9fb9952f319089e6a52b488e596de975e3b9f74b
Titanium2099 <Titanium2099>
2023-05-08 20:51:42 -0700

    adding comments

--- Connect4.py
+++ Connect4.py
@@ -1,20 +1,20 @@
-import numpy as np
-from colorama import init, Fore, Style
+import numpy as np #use numpy for board creation/manipulation
+from colorama import init, Fore, Style #use colorama to style terminal output
 
-ROW_COUNT = 6
-COLUMN_COUNT = 7
+ROW_COUNT = 6 # number of rows in the board
+COLUMN_COUNT = 7 # number of columns in the board
 
-def create_board():
-    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
+def create_board(): # create a new board
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT)) # a neat way to create a 2 dimensional array of zeros
     return board
 
 def drop_piece(board, row, col, piece):
     board[row][col] = piece
 
-def is_valid_location(board, col):
+def is_valid_location(board, col): #check if the player's requested move is valid
     return board[ROW_COUNT-1][col] == 0
 
-def get_next_open_row(board, col):
+def get_next_open_row(board, col): #get the next open row in the column
     for r in range(ROW_COUNT):
         if board[r][col] == 0:
             return r
@@ -23,13 +23,13 @@ def get_next_open_row(board, col):
 #    print(np.flip(board, 0))
 def print_board(board):
     # Flip the board vertically to start from the bottom
-    flipped_board = np.flip(board, 0)
+    flipped_board = np.flip(board, 0) #use numpy's flip function to flip the board vertically
 
     # Print the column indices
-    print("  ", end="")
-    for i in range(COLUMN_COUNT):
+    print("  ", end="") #using end 
+    for i in range(COLUMN_COUNT): #
         print(Fore.BLUE + f" {i}  " + Style.RESET_ALL, end="")
-    print()
+    print() #start a new line
 
     # Print the rows
     for r in range(ROW_COUNT):
@@ -40,9 +40,9 @@ def print_board(board):
             if piece == 0:
                 #print(" |   ", end="")
                 print(Fore.BLUE + " |   " + Style.RESET_ALL, end="")
-            elif piece == 1:
+            elif piece == 1: # Player 1's piece
                 print(Fore.BLUE + " |" + Fore.RED + " X " + Style.RESET_ALL, end="")
-            elif piece == 2:
+            elif piece == 2: # Player 2's piece
                 print(Fore.BLUE + " |" + Fore.YELLOW + " 0 " + Style.RESET_ALL, end="")
         print(Fore.BLUE + " |" + Style.RESET_ALL)
 
@@ -74,11 +74,11 @@ def winning_move(board, piece):
             if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
                 return True
 
-def draw_game_over(board):
+def draw_game_over(board): # draw the board when the game is over
     print_board(board)
-    print("Game Over!")
+    print("Game Over!") 
 
-def connect_4():
+def connect_4(): #  main function
     board = create_board()
     game_over = False
     turn = 0
@@ -87,8 +87,17 @@ def connect_4():
         # Player 1 input
         if turn == 0:
             print("Player 1's turn")
-            col = int(input("Make your selection (1-7): "))
+            col = input("Make your selection (1-7): ")
+            #check if the input is a number
+            if not col.isdigit():
+                print("Invalid input!")
+                continue
+            col = int(col)
             col -= 1
+            #check if number is in range
+            if col < 0 or col > 6:
+                print("Invalid input!")
+                continue
             if is_valid_location(board, col):
                 row = get_next_open_row(board, col)
                 drop_piece(board, row, col, 1)
@@ -100,8 +109,17 @@ def connect_4():
         # Player 2 input
         else:
             print("Player 2's turn")
-            col = int(input("Make your selection (1-7): "))
+            col = input("Make your selection (1-7): ")
+            #check if the input is a number
+            if not col.isdigit():
+                print("Invalid input!")
+                continue
+            col = int(col)
             col -= 1
+            #check if number is in range
+            if col < 0 or col > 6:
+                print("Invalid input!")
+                continue
             if is_valid_location(board, col):
                 row = get_next_open_row(board, col)
                 drop_piece(board, row, col, 2)
@@ -111,16 +129,16 @@ def connect_4():
                     game_over = True
 
         print_board(board)
-
+ 
         # Switch turn
         turn += 1
         turn %= 2
 
         # Check for tie
-        if np.count_nonzero(board == 0) == 0:
+        if np.count_nonzero(board == 0) == 0: #uses numpy to check if there are any empty spaces (0s) left
             draw_game_over(board)
             print("It's a tie!")
             game_over = True
 
-connect_4()
+connect_4() #run main function
 
